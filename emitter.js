b38fb789e01dd9623e5e46584c57a0a312354cf2
evgenyzinoviev <me@ch1p.com>
2016-02-29 02:22:06 +0200

    fixed tabs

--- emitter.js
+++ emitter.js
@@ -7,26 +7,26 @@ class Emitter {
     } else {
       this._emitterEvents[event].push(callback)
     }
-	}
+  }
 
   unbind(event, callback) {
-		if (!(event in this._emitterEvents)) {
+    if (!(event in this._emitterEvents)) {
       return
     }
-		this._emitterEvents[event].splice(this._emitterEvents[event].indexOf(callback), 1)
+    this._emitterEvents[event].splice(this._emitterEvents[event].indexOf(callback), 1)
     if (!this._emitterEvents[event].length) {
       delete this._emitterEvents[event]
     }
-	}
+  }
 
-	emit(event, ...args) {
-		if (!(event in this._emitterEvents)) {
+  emit(event, ...args) {
+    if (!(event in this._emitterEvents)) {
       return
     }
-		for (let i = 0; i < this._emitterEvents[event].length; i++) {
-			this._emitterEvents[event][i].apply(this, args);
-		}
-	}
+    for (let i = 0; i < this._emitterEvents[event].length; i++) {
+      this._emitterEvents[event][i].apply(this, args);
+    }
+  }
 }
 
 export default Emitter
